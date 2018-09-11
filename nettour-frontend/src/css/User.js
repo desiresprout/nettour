696a6263e1b6f44711110eafd7b2620d26df8b19
desiresprout <desiresprout@gmail.com>
2018-09-12 02:03:11 +0900

    user

---
+++ nettour-frontend/src/css/User.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import oc from 'open-color';
+import { shadow, media } from 'css/styleUtil';
+import { Link } from 'react-router-dom';
+
+
+
+export const UserWrapper = styled.div`
+        display : flex;
+        flex-direction : row;
+`;
+
+export const SidebarWrapper = styled.div`
+    display : flex;
+    flex-direction : column;
+    width : 15rem;
+    border : 1px solid red;
+    height : 100vh;
+    justify-content : flex-start;
+    margin-top : 8rem;
+`;
+
+
+export const SidebarContainer = styled.div`
+    margin : 0 auto;
+
+`;
+
+
+export const HeaderWrapper = styled.div`
+
+`;
+
+export const HeaderContainer = styled.div`
+    
+`;
